b7a08985ad0dbabb2322f0ef8bd146d42fdca665
Ethan Moody <moodyssbm@gmail.com>
2018-04-12 15:54:41 -0400

    not finished with 8x8 sim yet

--- docs/8x8sim/js/palettes.js
+++ docs/8x8sim/js/palettes.js
@@ -1,1 +1,25 @@
-let palettes = {};
+let wht = 'rgb(255,255,255)';
+let brt = 'rgb(175,175,175)';
+let drk = 'rgb(88,88,88)';
+let blk = 'rgb(0,0,0)';
+let blu = 'rgb(0,0,255)';
+let ylw = 'rgb()';
+
+let palettes = {
+	bg0: [],
+	bg1: [],
+	bg2: [],
+	bg3: [],
+	bg4: [],
+	bg5: [],
+	bg6: [],
+	bg7: [],
+	obj0: [],
+	obj1: [],
+	obj2: [],
+	obj3: [],
+	obj4: [],
+	obj5: [],
+	obj6: [],
+	obj7: [],
+};
